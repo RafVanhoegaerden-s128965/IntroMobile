98f4b4aa67dd7b6d933cf8508db92c92fa90b446
akaHann <bayramcapa85@gmail.com>
2023-04-24 18:34:30 +0200

    Changed auth of the app from Firebase Auth to own login system

---
+++ parq_app/lib/views/register_view.dart
@@ -0,0 +1,149 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:parq_app/views/login_view.dart';
+
+import '../constants/routes.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  void _register() async {
+    final username = _usernameController.text;
+    final email = _emailController.text;
+    final password = _passwordController.text;
+    final confirmPassword = _confirmPasswordController.text;
+
+    if (_formKey.currentState!.validate() && password == confirmPassword) {
+      final hashedPassword = sha256.convert(utf8.encode(password)).toString();
+
+      final usersRef = FirebaseFirestore.instance.collection('users');
+      final existingUserWithEmail =
+          await usersRef.where('email', isEqualTo: email).get();
+
+      final existingUserWithUsername =
+          await usersRef.where('username', isEqualTo: username).get();
+
+      if (existingUserWithEmail.docs.isNotEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Een gebruiker met dezelfde e-mailadres bestaat al.'),
+          ),
+        );
+      } else if (existingUserWithUsername.docs.isNotEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Een gebruiker met dezelfde username bestaat al.'),
+          ),
+        );
+      } else {
+        // Voeg de nieuwe gebruiker toe aan de database
+        await usersRef.add({
+          'id': FirebaseFirestore.instance.collection('users').doc().id,
+          'username': username,
+          'email': email,
+          'password': hashedPassword
+        });
+
+        // Display a snackbar to indicate that registration was successful.
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Registration successful.'),
+          ),
+        );
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const LoginView()));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(labelText: 'Username'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a username.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an email address.';
+                    }
+                    if (!value.contains('@') || !value.contains('.')) {
+                      return 'Please enter a valid email address.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a password.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                    controller: _confirmPasswordController,
+                    obscureText: true,
+                    decoration:
+                        const InputDecoration(labelText: 'Confirm Password')),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _register,
+                  child: const Text('Register'),
+                ),
+                //Button back
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+                  },
+                  child: const Icon(Icons.arrow_back),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
